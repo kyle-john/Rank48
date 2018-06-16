97fe117914337ea7b0150ab4940d2126336aadd0
Kyle John <njk230@gmail.com>
2018-06-17 01:06:35 +0900

    Show trainees

--- Rank48/MainPage.xaml.cs
+++ Rank48/MainPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,9 +7,22 @@ namespace Rank48
 {
     public partial class MainPage : ContentPage
     {
+        Task initTask;
+
         public MainPage()
         {
             InitializeComponent();
+
+            initTask = Task.Run(async () =>
+            {
+                await Produce48Manager.Instance.InitializeAsync();
+            });
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await initTask;
+            listView.ItemsSource = Produce48Manager.Instance.Trainees.Values;
         }
     }
 }
