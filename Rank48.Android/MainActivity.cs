d1ae4367fe6641e5435a7c246dac082e66fdd921
Kyle John <njk230@gmail.com>
2018-06-23 05:33:45 +0900

    Update Android project

--- Rank48.Android/MainActivity.cs
+++ Rank48.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms;
 
 namespace Rank48.Droid
 {
-    [Activity(Label = "Rank48", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Rank 48", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
